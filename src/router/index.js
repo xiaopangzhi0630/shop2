209d4847df0de4d9976f42465f1fe762a8d5d8f2
xiaopangzhi <445917591@qq.com>
2019-02-14 18:14:30 +0800

    登录-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Login from '@/components/login.vue'
+import Home from '@/components/home.vue'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
       path: '/',
-      name: ''
+      name: 'home',
+      component: Home
     }
   ]
 })
