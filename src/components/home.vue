6d04b93192969c6cba9dffaf61e84829ff870580
xiaopangzhi <445917591@qq.com>
2019-02-15 15:05:05 +0800

    token修改-登录提示

--- src/components/home.vue
+++ src/components/home.vue
@@ -6,7 +6,7 @@
                     <img src="../assets/logo.png" alt="图片记载失败">
                 </el-col>
                 <el-col :span="19" class="middle">
-                    <h2>品优购后台管理系统</h2>
+                    <h2 style="color:#fff">品优购后台管理系统</h2>
                 </el-col>
                 <el-col :span="1">
                     <a href="#" class="loginOut">退出</a>
@@ -21,28 +21,27 @@
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style>
 .container {
-    height: 100%;
-    background-color: #324152;
+  height: 100%;
+  background-color: #b3c0d1;
 }
 .aside {
-    background-color: yellow;
+  background-color: yellow;
 }
 .mian {
-    background-color: gray;
+  background-color: gray;
 }
 .loginOut {
-    line-height: 60px;
-    margin: 0 auto;
-    color: ghostwhite;
-    text-decoration: none;
+  line-height: 60px;
+  margin: 0 auto;
+  text-decoration: none;
 }
 .middle {
-    line-height: 60px;
-    text-align: center;
+  line-height: 60px;
+  text-align: center;
 }
 </style>
