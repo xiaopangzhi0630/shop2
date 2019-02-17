2a12d0cb64528ad6c603e4051c7101a0bad2862e
xiaopangzhi <445917591@qq.com>
2019-02-17 09:57:09 +0800

    权限列表-表格层级完善

--- src/components/rights.vue
+++ src/components/rights.vue
@@ -1,17 +1,24 @@
 <template>
-    <el-card class="box">
-        <!-- 面包屑 -->
-        <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
-        <!-- 表格 -->
+  <el-card class="box">
+    <!-- 面包屑 -->
+    <cus-bread level1="权限管理" level2="权限列表"></cus-bread>
+    <!-- 表格 -->
 
-        <!-- // id 权限 ID // authName 权限说明 // level 权限层级 // pid 权限父 ID // path 对应访问路径 -->
-        <el-table height="450px" border stripe :data="list" style="width: 100%" class="tab">
-            <el-table-column type="index" prop="id" label="#" width="100"></el-table-column>
-            <el-table-column prop="authName" label="角色名称" width="320"></el-table-column>
-            <el-table-column prop="path" label="路径" width="326"></el-table-column>
-            <el-table-column prop="level" label="层级" width="320"></el-table-column>
-        </el-table>
-    </el-card>
+    <!-- // id 权限 ID // authName 权限说明 // level 权限层级 // pid 权限父 ID // path 对应访问路径 -->
+    <el-table height="450px" border stripe :data="list" style="width: 100%" class="tab">
+      <el-table-column type="index" prop="id" label="#" width="100"></el-table-column>
+      <el-table-column prop="authName" label="角色名称" width="320"></el-table-column>
+      <el-table-column prop="path" label="路径" width="326"></el-table-column>
+      <!-- 该区域显示显示的内容不是prop文本内容, 则使用template -->
+      <el-table-column prop="level" label="层级" width="320">
+        <template slot-scope="scope">
+          <span v-if="scope.row.level==='0'">一级</span>
+          <span v-if="scope.row.level==='1'">二级</span>
+          <span v-if="scope.row.level==='2'">三级</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
