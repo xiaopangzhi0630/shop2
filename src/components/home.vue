d0338acf2f42d2063790d62343538446fd9c595b
xiaopangzhi <445917591@qq.com>
2019-02-18 21:29:37 +0800

    动态获取menus

--- src/components/home.vue
+++ src/components/home.vue
@@ -19,73 +19,18 @@
       <el-aside class="aside" width="200px">
         <!-- router 开启路由模式,点击对应的导航, 当前的path就是激活导航index的值 -->
         <el-menu default-active="1" :router="true" :unique-opened="true">
-          <!-- 用户管理 -->
-          <el-submenu index="1">
+          <!-- 用户管理  -->
+          <el-submenu :index="item1.order+''" v-for="(item1,i) in menus" :key="item1.id">
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{item1.authName}}</span>
             </template>
-            <el-menu-item index="users">
+            <el-menu-item :index="item2.path+''" v-for="(item2,i) in item1.children" :key="item2.id">
               <i class="el-icon-menu"></i>
-              用户列表
+              {{item2.authName}}
             </el-menu-item>
           </el-submenu>
-          <!-- 权限管理 -->
-          <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="roles">
-              <i class="el-icon-menu"></i>
-              角色列表
-            </el-menu-item>
-            <el-menu-item index="rights">
-              <i class="el-icon-menu"></i>
-              权限列表
-            </el-menu-item>
-          </el-submenu>
-          <!-- 商品管理 -->
-          <el-submenu index="3">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>商品管理</span>
-            </template>
-            <el-menu-item index="1-1">
-              <i class="el-icon-menu"></i>
-              商品列表
-            </el-menu-item>
-            <el-menu-item index="1-1">
-              <i class="el-icon-menu"></i>
-              分类参数
-            </el-menu-item>
-            <el-menu-item index="1-1">
-              <i class="el-icon-menu"></i>
-              商品分类
-            </el-menu-item>
-          </el-submenu>
-          <!-- 订单管理 -->
-          <el-submenu index="4">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>订单管理</span>
-            </template>
-            <el-menu-item index="orders">
-              <i class="el-icon-menu"></i>
-              订单列表
-            </el-menu-item>
-          </el-submenu>
-          <!-- 数据统计 -->
-          <el-submenu index="5">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>数据统计</span>
-            </template>
-            <el-menu-item index="reports">
-              <i class="el-icon-menu"></i>
-              数据列表
-            </el-menu-item>
-          </el-submenu>
+
         </el-menu>
       </el-aside>
       <el-main class="mian">
@@ -99,6 +44,7 @@
 export default {
   data() {
     return {
+      menus: [],
       loginUser: localStorage.getItem("logigUserName")
     };
   },
@@ -114,7 +60,20 @@ export default {
       });
     }
   },
+  created() {
+    this.getMenus();
+  },
   methods: {
+    // 动态获取目录
+    async getMenus() {
+      const res = await this.$http.get(`menus`);
+      // console.log(res.data.data);
+      const { data, meta: { msg, status } } = res.data;
+      if (status === 200) {
+        this.menus = data;
+      }
+    },
+
     // 返回首页
     backHome() {
       this.$router.push({
