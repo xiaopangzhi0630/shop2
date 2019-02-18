6422ed8edfec25d5bc7929e9d44a322807d18672
xiaopangzhi <445917591@qq.com>
2019-02-18 20:56:22 +0800

    权限修改

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -55,6 +55,17 @@
       </el-table-column>
     </el-table>
 
+    <!-- 对话框  权限分配 -->
+
+    <el-dialog title="权限分配" :visible.sync="dialogFormVisible">
+      <el-tree ref="treeDom" :data="treedata" show-checkbox node-key="id" default-expand-all :default-checked-keys="arrCheck" :props="defaultProps">
+      </el-tree>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="setRights()">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </el-card>
 </template>
 
@@ -62,13 +73,85 @@
 export default {
   data() {
     return {
-      roles: []
+      roles: [],
+      dialogFormVisible: false,
+      // 树形结构绑定的数据
+      treedata: [],
+      // 默认展开的节点(被展开节点的id)   HTML中删除:default-expanded-keys="arrExpand"属性
+      // arrExpand: [],
+      // 默认选中的节点(被选中节点的id)
+      arrCheck: [],
+      defaultProps: {
+        label: "authName",
+        children: "children"
+      },
+      // 当前角色id
+      currRoleId: -1
     };
   },
   created() {
     this.getRoles();
   },
   methods: {
+    // 更改权限
+    async setRights() {
+      // 在js中调用组件标签的js方法  -> ref操作dom
+      // 1. 给要操作的页面元素el-tree设置ref="值"
+      // 2. 在js中通过this.$refs.ref的值.js方法
+
+      // 获取半选节点组成的数组
+      const arr1 = this.$refs.treeDom.getHalfCheckedKeys();
+      // console.log(arr1);
+
+      // 获取全选节点组成的数组
+      const arr2 = this.$refs.treeDom.getCheckedKeys();
+      // console.log(arr2);
+      // ES6 展开操作运算符
+      const arr = [...arr1, ...arr2];
+      // 发送请求
+      const res = await this.$http.post(`roles/${this.currRoleId}/rights`, {
+        rids: arr.join(",")
+      });
+      // console.log(res);
+      const { data, meta: { msg, status } } = res.data;
+
+      if (status === 200) {
+        this.dialogFormVisible = false;
+        // 刷新列表
+        this.getRoles();
+        this.$message.success("权限修改成功!");
+      }
+    },
+
+    // 点击显示权限设置
+    async showDiaSetRights(role) {
+      // 设置角色id,后面提交会使用
+      this.currRoleId = role.id;
+      this.dialogFormVisible = true;
+      // 发送请求获取所有权限
+      const res = await this.$http.get(`rights/tree`);
+      // console.log(res.data.data);
+      const { data, meta: { msg, status } } = res.data;
+      if (status === 200) {
+        this.treedata = data;
+      }
+
+      // 获取当前角色的已有的权限
+      const temp = [];
+      role.children.forEach(item1 => {
+        // temp.push(item1.id);
+        item1.children.forEach(item2 => {
+          // temp.push(item2.id);
+          item2.children.forEach(item3 => {
+            temp.push(item3.id);
+          });
+        });
+      });
+
+      // console.log(temp);
+      this.arrCheck = temp;
+    },
+
     // 删除角色
     async deleteRights(role, rights) {
       const res = await this.$http.delete(
@@ -81,22 +164,17 @@ export default {
         // 刷新数据
         role.children = data;
         // console.log(role);
-        
-        console.log(role.children);
-        
+        // console.log(role.children);
       }
     },
-
-    // 点击显示权限设置
-    showDiaSetRights() {},
 
     // 获取角色
     async getRoles() {
       const res = await this.$http.get(`roles`);
-      console.log(res);
+      // console.log(res);
       const { data, meta: { msg, status } } = res.data;
       this.roles = data;
-      console.log(this.roles);
+      // console.log(this.roles);
     }
   }
 };
